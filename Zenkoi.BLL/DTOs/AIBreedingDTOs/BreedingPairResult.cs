50c49b0bb2993f0565567803f1cf475591c20236
Van Quoc Thinh <thinhvan.231003@gmail.com>
2025-10-22 08:50:43 +0700

    update response BreedingDTO

---
+++ Zenkoi.BLL/DTOs/AIBreedingDTOs/BreedingPairResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zenkoi.BLL.DTOs.AIBreedingDTOs
+{
+    public class BreedingPairResult
+    {
+        public int MaleId { get; set; }
+        public int FemaleId { get; set; }
+        public string Reason { get; set; } = "";
+        public double PredictedFertilizationRate { get; set; }
+        public double PredictedHatchRate { get; set; }
+        public double PredictedSurvivalRate { get; set; }
+        public double PredictedHighQualifiedRate { get; set; }
+        public double PatternMatchScore { get; set; } 
+        public double PercentInbreeding { get; set; }
+        public int Rank { get; set; }             
+    }
+}
---
+++ Zenkoi.BLL/DTOs/AIBreedingDTOs/BreedingResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zenkoi.BLL.DTOs.AIBreedingDTOs
+{
+    public class BreedingResponseDTO
+    {
+        public List<BreedingPairResult> RecommendedPairs { get; set; } = new();
+    }
+}
